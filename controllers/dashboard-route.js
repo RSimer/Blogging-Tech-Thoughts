77b95867ac98613dad66a748957136e9a7a80846
RSimer <65471088+RSimer@users.noreply.github.com>
2021-08-15 15:10:29 -0700

    routes are finally done I think, and added handlebars files which I think are also right. honestly have no idea what I'm doing I'm scared

---
+++ controllers/dashboard-route.js
@@ -0,0 +1,80 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try{
+   const postData = await Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'post_body',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    const postMap = postData.map((post)=>{
+        post.get({plain:true});
+        res.render('dashboard',{postMap, logged_in:true});
+    })
+ }catch (err){
+    res.status(500).json(err);
+}
+});
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+    try{
+    const postEdit =  await Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_body',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    });
+    const post = postEdit.get({plain:true});
+    res.render('edit-post',{
+        post,
+        logged_in: true
+    });
+}catch{
+    res.status(500).json(err);
+}
+router.get('/createPost', withAuth, (req, res) => {
+    
+    res.render('create-post');
+})
+});
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,23 +4,94 @@ const {Post, Comment, User} = require('../models/');
 router.get('/',async (req,res) =>{
 
     try{
-        Post.findAll({
+
+       const postAttr = await Post.findAll({
             attributes: [
                 'id',
                 'title',
                 'postURL',
                 'createdAt'
             ],
-
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id','commentText','post_id','user_id','createdAt'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
         })
-    } catch(err){
+        const postMap = postAttr.map((post) =>{
+            return post.get({plain: true});
+        });
 
+        res.render('homepage',{
+            postMap,
+            logged_in: req.session.logged_in
+
+        });
+    } catch(err){
+        res.status(400).json(err);
     }
 
 });
 
 router.get('/post/:id', async (req,res) =>{
 
+    try{
+        const postAttr = await Post.findByPk(req.params.id,{
+            attributes: [
+                'id',
+                'title',
+                'postURL',
+                'createdAt'
+            ],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id','commentText','post_id','user_id','createdAt'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+        const post = postAttr.get({ plain: true });
+
+        res.render('post', {
+          ...post,
+          logged_in: req.session.logged_in
+        });
+
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login',(req,res)=>{
+    if(req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+router.get('/signup',(req,res)=>{
+    if(req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('sign-up');
 });
 
 module.exports = router;
